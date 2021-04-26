a8cceef7ee0891ef8d8d8243bd85c998f4862515
DIVYANKAR BHARGAV <divyankar.bhargav@supista.com>
2021-04-26 16:45:15 +0530

    Added forms validation for volunteers

--- assets/js/volunteer.js
+++ assets/js/volunteer.js
@@ -11,8 +11,27 @@ form5.addEventListener('submit', (event) => {
     const purpose = form5.elements['purpose'].value;
     const serve = form5.elements['serve'].value;
 
-    const volunteerDetails = { name, mobile, gender, age, email, purpose, serve };
+    const volunteerDetails = {
+        apiID: "Volunteers-Volunteers-Create-Save-create",
+        data: {
+            Name: name,
+            Email: email,
+            Mobile: mobile,
+            Age: age,
+            Gender: gender,
+            WhydoyouwanttoVolunteer: purpose,
+            Howcanyouserve: serve
+        }
+    }
 
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://supista.com/api/1/6083f8fdce57100e4e479450", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhttp.setRequestHeader('Acces-Control-Allow-Methods', 'POST');
+    xhttp.setRequestHeader('Content-Type', 'Access-Control-Allow-Origin', 'Acces-Contorl-Allow-Methods');
+    xhttp.send(JSON.stringify(volunteerDetails));
     console.log(volunteerDetails);
+
     form5.reset();
 });
